34334f2881ec2012eaa13ed509323599c23ddf7d
YBiheza <bihezayanita@gmail.com>
2025-11-15 15:31:21 +0300

    feat: add filter function to sort dishes by category

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-export function Button ({ onClick, type = "button", disabled = false, label = 'Dessert'}) {
+export function Button ({ onClick, type = "button", disabled = false, label, current}) {
   return (
     <button
       type={type}
-      className={`${styles.buttonStyles}`}
+      className={`${styles.buttonStyles} ${current === true ? styles.currButton : ''}`}
       onClick={onClick}
       disabled={disabled}
     >
